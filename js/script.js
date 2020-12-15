7c55fc46d98ab7057709d21e8ce43e4befdbbe0b
Federico <crudofederico@gmail.com>
2020-12-15 19:02:42 +0100

    update

--- js/script.js
+++ js/script.js
@@ -7,14 +7,14 @@ var element = document.getElementById('richiedi');
     var eta=document.getElementById("eta").value;
     var costo=0.21;
     var carrozza=Math.floor(Math.random() * 9) + 1;
-    var codicecp=Math.floor(Math.random() * 99999) +90000;
-    var pbase=(km*costo).toFixed(2);
+    var codicecp=Math.floor(Math.random() * (9999 )+90000);
+    var pbase=(km*costo);
     var sconto20=pbase*0.20;
     var sconto40=pbase*0.40;
     var offerta="";
-    var cbiglietto="";
+    var cbiglietto=""
     //controllo campi
-    if(nome==""||km=="" || eta==""){
+    if(nome=="" || km=="" || eta==""){
          carrozza='';
          codicecp='';
         document.getElementById('message').innerHTML= '<span id="text"><span></i>';
@@ -43,15 +43,30 @@ var element = document.getElementById('richiedi');
             offerta="Tarriffa Standard";
         }
             
-        
+         //caricamento dati
+    document.getElementById("nomeP").innerHTML=nome;  
+    document.getElementById("offerta").innerHTML=offerta;  
+    document.getElementById("carrozza").innerHTML=carrozza;  
+    document.getElementById("codicecp").innerHTML=codicecp;  
+    document.getElementById("costo").innerHTML=cbiglietto.toFixed(2);   
+    document.getElementById("box-biglietto").className='show box m_top_10  pd_20';   
     }
-    //caricamento dati
-    document.getElementById("left-information").innerHTML+='<span>'+nome+'</span>';  
-    document.getElementById("data").innerHTML+='<td>'+offerta+'</td>';  
-    document.getElementById("data").innerHTML+='<td>'+carrozza+'</td>';  
-    document.getElementById("data").innerHTML+='<td>'+codicecp+'</td>';  
-    document.getElementById("data").innerHTML+='<td>'+cbiglietto+'</td>';  
+  
     }    
-);         
+);   
+var element = document.getElementById('annulla');
+  element.addEventListener('click',
+  function() {
+    document.getElementById("nome").value='';
+    document.getElementById("km").value='';
+    document.getElementById("eta").value='';
+    document.getElementById("nomeP").innerHTML='';  
+    document.getElementById("offerta").innerHTML='';  
+    document.getElementById("carrozza").innerHTML='';  
+    document.getElementById("codicecp").innerHTML='';  
+    document.getElementById("costo").innerHTML='';  
+    document.getElementById("box-biglietto").className='hidden';   
+    }    
+);      
    
 
